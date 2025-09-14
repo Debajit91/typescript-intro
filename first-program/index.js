ad2fdf4b84117fcf85662b7ab5eee67981bdf5e7
Debajit91 <debajitroy544@gmail.com>
2025-09-14 21:46:27 +0600

    first program typescript run

--- first-program/index.js
+++ first-program/index.js
@@ -1,2 +1,6 @@
 var userName = 'Debajit Roy';
 console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,2 +1,7 @@
 let userName = 'Debajit Roy';
 console.log(userName);
+
+function addNumbers (num1:number, num2:number){
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
