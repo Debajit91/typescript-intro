e4b050692c753246301cba84bf4ffe80347ae83b
Debajit91 <debajitroy544@gmail.com>
2025-09-15 20:18:15 +0600

    tuple-type explored

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+// mixed data - key, value
+var user;
+user = [101, "Dev"];
+user.push(102, "Saran");
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mixed data - key, value
+let user: [number, string];
+user = [101, "Dev"]
+user.push(102, "Saran")
+
+
+
+console.log(user);
